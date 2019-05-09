140e31c7aed9d9515abf875c3e47c0ed2dc07df2
Braden Arestides <barestides@gmail.com>
2019-05-09 15:32:32 -0400

    Add input parser and sorting functions.

--- src/record_sorter/core.clj
+++ src/record_sorter/core.clj
@@ -1,17 +1,13 @@
 (ns record-sorter.core
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [clj-time.core :as t]
             [clj-time.format :as f]
-            [clj-time.coerce :as c])
+            [clj-time.coerce :as c]
+            [faconne.core :as faconne])
   (:gen-class))
 
 (def date-formatter (f/formatter "MM/dd/yyyy"))
-(def file-configs [{:file-name "pipe-delimited"
-                    :delimiter " | "}
-                   {:file-name "comma-delimited"
-                    :delimiter ", "}
-                   {:file-name "space-delimited"
-                    :delimiter " "}])
 
 (defn rand-birth-date
   [time-before]
@@ -33,12 +29,51 @@
 
 (defn gen-input-files
   [records-per-file]
-  (doseq [{:keys [file-name delimiter]} file-configs]
-    (let [records (repeatedly records-per-file gen-record)
-          str-records (map (comp (partial apply str)
-                                 (partial interpose delimiter))
-                           records)]
-      (spit (io/file "resources" file-name) (apply str (interpose "\n" str-records))))))
+  (let [file-configs [{:file-name "pipe-delimited"
+                       :delimiter " | "}
+                      {:file-name "comma-delimited"
+                       :delimiter ", "}
+                      {:file-name "space-delimited"
+                       :delimiter " "}]]
+       (doseq [{:keys [file-name delimiter]} (vals file-configs)]
+         (let [records (repeatedly records-per-file gen-record)
+               str-records (map (comp (partial apply str)
+                                      (partial interpose delimiter))
+                                records)]
+           (spit (io/file "resources" file-name) (apply str (interpose "\n" str-records)))))))
+
+(defonce records-db (atom []))
+
+(defn parse-file
+  [file delimiter-pattern]
+  (let [string-records (string/split-lines (slurp file))
+        vector-records (map #(string/split % delimiter-pattern) string-records)]
+    (faconne/transform vector-records
+                       [[lname fname gender color bday]]
+                       [{:last-name lname
+                         :first-name fname
+                         :gender gender
+                         :color color
+                         :birthday bday}])))
+
+(defn records-by-gender
+  []
+  (sort-by (juxt :gender :last-name) @records-db))
+
+(defn records-by-birthdate
+  []
+  (sort-by #(f/parse date-formatter (:birthday %)) @records-db))
+
+(defn records-by-lastname-descending
+  []
+  (sort-by :last-name #(compare %2 %1) @records-db))
+
+(defn parse-record-files-and-add-to-db!
+  [pipe-filename comma-filename space-filename]
+  (let [records (concat (parse-file pipe-filename #" \| ")
+                        (parse-file comma-filename #", ")
+                        (parse-file space-filename #" "))]
+    (reset! records-db records)))
 
 (defn -main
   "I don't do a whole lot ... yet."
